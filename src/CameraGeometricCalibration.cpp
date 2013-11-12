664952ac69035b4b3c51e1aaf4df3ce52623586c
Jetze Baumfalk <jetze@baumfalk.nl>
2013-11-12 18:34:26 +0100

    added screen that displays webcam output

---
+++ src/CameraGeometricCalibration.cpp
@@ -0,0 +1,42 @@
+/*
+ * CameraGeometricCalibration.cpp
+ *
+ *  Created on: 12 nov. 2013
+ *      Author: Jetze
+ */
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+// Displays a supplied image
+int main(int argc, const char** argv) {
+
+	VideoCapture camera;
+	Mat webcamImage;
+	camera.open(0);
+	if (!camera.isOpened()) {
+		cerr << "ERROR: Could not access the camera or video!" << endl;
+		exit(1);
+	}
+
+	namedWindow("test", CV_WINDOW_AUTOSIZE);
+	while (true) {
+		camera >> webcamImage;
+		if (webcamImage.empty()) {
+			cerr << "ERROR: Couldn't grab a camera frame." << endl;
+			exit(1);
+		}
+		//rotate it on the x axis
+		flip(webcamImage, webcamImage, 1);
+		imshow("test", webcamImage);
+		if (27 == waitKey(33))
+			break;
+	}
+	return 0;
+}
+
