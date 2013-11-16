5c741972101d6d8594fab94b30dc5b0254e74c40
SpidersBro <barend.poot@gmail.com>
2013-11-16 15:15:19 +0100

    Revert 98a5ab1..31691ea
    This rolls back to commit 98a5ab1a0464244c55d833ac54042b85f5055a74.

--- src/CameraGeometricCalibration.cpp
+++ src/CameraGeometricCalibration.cpp
@@ -1,13 +1,15 @@
-#include <iostream>
-#include <sstream>
-#include <time.h>
-#include <stdio.h>
+/*
+ * CameraGeometricCalibration.cpp
+ *
+ *  Created on: 12 nov. 2013
+ *      Author: Jetze en Barend
+ */
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
-
+#include <iostream>
 using namespace cv;
 using namespace std;
 
@@ -29,11 +31,32 @@ int main(int argc, const char** argv) {
 			cerr << "ERROR: Couldn't grab a camera frame." << endl;
 			exit(1);
 		}
+        
+        // Vector
+        vector<Point2f> pointBuf;
+
+        // set boardSize
+        Size boardSize;
+        boardSize.width = 9;
+        boardSize.height = 6;
+        
 		//rotate it on the x axis
 		flip(webcamImage, webcamImage, 1);
 		imshow("test", webcamImage);
+        
+        // Find a chessboard pattern on webcamfeed with size boardSize.
+        bool found;
+        found = findChessboardCorners( webcamImage, boardSize, pointBuf,
+                                      CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FAST_CHECK | CV_CALIB_CB_NORMALIZE_IMAGE);
+        
+        if (found)
+        {
+            cout << "found" << endl;
+        }
+        
 		if (27 == waitKey(33))
 			break;
 	}
 	return 0;
 }
+
